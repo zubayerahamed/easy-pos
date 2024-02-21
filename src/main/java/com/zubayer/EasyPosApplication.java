bf5bc5d26472b57caa868dfc27ce4afbb5c7f589
Zubayer Ahamed <zubayerahamed.freelancer@gmail.com>
2024-02-21 08:45:32 +0600

    Project Init

---
+++ src/main/java/com/zubayer/EasyPosApplication.java
@@ -0,0 +1,13 @@
+package com.zubayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zubayer/EasyPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zubayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
